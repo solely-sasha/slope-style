2a8b3685fd611d44d1a1242829551c9c768e95e9
sasha sims <solely.sashasims@gmail.com>
2023-02-06 15:31:45 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Use the rest operator to help return an array of animals, no matter how many animals are passed to it
+
+ function collectAnimals(/*your code here*/) {
+    /*and here*/
+}
+
+collectAnimals("dog", "cat", "mouse", "jackolope", "platypus");
+// ["dog", "cat", "mouse", "jackolope", "platypus"]
+
